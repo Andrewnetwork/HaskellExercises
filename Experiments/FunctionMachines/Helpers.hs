93e71b30681087e4557ccf90b8eab012848f6b42
Andrew Ribeiro <andrewnetwork@gmail.com>
2018-12-24 01:43:33 -0500

    Cleaning up the repo a bit.

--- Experiments/FunctionMachines/Helpers.hs
+++ Experiments/FunctionMachines/Helpers.hs
@@ -1,10 +1,19 @@
+-- Helpers.hs
+-- Andrew Ribeiro
+-- December 2018
+
 module Helpers where
 
 nCartesian :: Foldable t => t [a] -> [[a]]
 nCartesian xss = foldr f [[]] xss
                  where f l a = [ x:xs | x <- l, xs <- a ]
 
+-- ####### Datasets ###########
+makeDataset :: (Num a, Enum a) => (a -> b) -> a -> [(a, b)]
+makeDataset fn maxNum = zip [0..maxNum] $ fn <$> [0..maxNum]
+
 sumOfNat n = (n*(n+1))/2
-sumOfNatLs maxNum = zip [0.0..maxNum] (map sumOfNat [0..maxNum])
+sumOfNatLs maxNum = makeDataset sumOfNat maxNum
+
 sumOfSumOfNat n = (n*(n+1)*(n+2)*(n+3))/24
-sumOfSumOfNatLs maxNum = zip [0.0..maxNum] (map sumOfSumOfNat [0..maxNum])
+sumOfSumOfNatLs maxNum = makeDataset sumOfSumOfNat maxNum
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# HaskellExercises
+# Haskell Exercises
+Hello and welcome to my Haskell exercises! This repo is a somewhat disordered
+dumping ground for byproducts of my Haskell education. I plan to someday corral
+this material into a tutorial on Haskell or perhaps something else educational.
+Stay tuned!
+
+Andrew Ribeiro 2018
+MIT License
